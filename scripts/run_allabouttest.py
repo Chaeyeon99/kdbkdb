c7a29029e1cb628114cedc545c01432930ea11f8
Chaeyeon <74848401+Chaeyeon99@users.noreply.github.com>
2025-06-08 15:09:24 +0900

    Update and rename run_fabfabfab.py to run_allabouttest.py

---
+++ scripts/run_allabouttest.py
@@ -0,0 +1,3 @@
+from all-about-test.core.engine import start_engine
+if __name__ == "__main__":
+    start_engine()
--- scripts/run_fabfabfab.py
+++
@@ -1,3 +0,0 @@
-from FabFabFab.core.engine import start_engine
-if __name__ == "__main__":
-    start_engine()
